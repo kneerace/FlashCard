99ff4107f12718ddf4b6918c6abe817ad318b7ec
kneerace <kneerace@gmail.com>
2022-06-20 10:01:03 -0400

    Added Link (view and Study) and Delete button to RenderDeck, as well utilized card bootstrap properties

--- src/components/RenderDecks.js
+++ src/components/RenderDecks.js
@@ -1,5 +1,7 @@
 import React, {useState, UseEffect, useEffect} from "react";
+import {Link} from "react-router-dom";
 import {listDecks} from "./../utils/api/index";
+
 
 function RenderDecks(){
     // setting up deck, default as blank Array
@@ -16,18 +18,30 @@ function RenderDecks(){
 
     const deckList = decks.map((deck)=>{
         const cards = deck.cards;
+    
 
         return(
             <div className="col-sm-6" key={deck.id}>
                 <div className="card">
-                    <div className="card-body d-flex justify-content-between">
-                        {/* deck title */}
-                        <h4 className="card-title">{deck.name}</h4>
-                        {/* cards number in a deck */}
-                        <p>{`${cards.length} cards`}</p>
+                    <div className="card-body">
+                        <div className="d-flex justify-content-between">
+                            {/* deck title */}
+                            <h4 className="card-title">{deck.name}</h4>
+                            {/* cards number in a deck */}
+                            <p>{`${cards.length} cards`}</p>
+                        </div>
+                        {/*  deck description */}
+                        <p className="card-text">{deck.description}</p>
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <Link to={`decks/${deck.id}`} className='btn btn-secondary'>View</Link>
+                                <Link to={`decks/${deck.id}/study`} className='btn btn-primary'>Study</Link>
+                            </div>
+                            <div>
+                                <button className='btn btn-danger'>Delete</button>
+                            </div>
+                        </div>
                     </div>
-                    {/*  deck description */}
-                    <p className="card-text">{deck.description}</p>
                 </div>
             </div>
         )
