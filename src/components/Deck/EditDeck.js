59437fa2430da90b05ad43deea111e485a491ceb
kneerace <kneerace@gmail.com>
2022-06-22 07:06:19 -0400

    EditDeck>> new component added with base code

---
+++ src/components/Deck/EditDeck.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function EditDeck(){
+
+    const EditDeckNaviLink =()=>{
+        return(
+            <nav>EditDeckNaviLink</nav>
+        )
+    }
+    return(
+        <EditDeckNaviLink />
+
+    )
+}
+
+export default EditDeck;
