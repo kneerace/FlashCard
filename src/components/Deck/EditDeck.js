acf7b570a746595cae2c76ca0a87879de7a18cc6
kneerace <kneerace@gmail.com>
2022-06-22 07:29:59 -0400

    EditDeck>>EditDeckNavLink updated

--- src/components/Deck/EditDeck.js
+++ src/components/Deck/EditDeck.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 function EditDeck(){
 
+    const {deckID} = useParams();
+    console.log(deckID);
+
     const EditDeckNaviLink =()=>{
         return(
-            <nav>EditDeckNaviLink</nav>
+            <nav aria-label="breadcurmb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li className="breadcrumb-item">Deckname</li>
+            </ol>
+        </nav>
         )
     }
     return(
-        <EditDeckNaviLink />
-
+        <>
+            <EditDeckNaviLink />
+            <h1>Edit Deck</h1>
+         </>
     )
 }
 
