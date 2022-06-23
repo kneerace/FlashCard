a555062f68663f37005f226bb8e522727a55f102
kneerace <kneerace@gmail.com>
2022-06-22 21:35:32 -0400

    EditDeck>>readDeck call added and CreateDeckForm called with props

--- src/components/Deck/EditDeck.js
+++ src/components/Deck/EditDeck.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { readDeck } from "../../utils/api";
+import CreateDeckForm from "../CreateDeck/CreateDeckForm";
 
 function EditDeck(){
 
+    const [deck, setDeck]= useState(null);
     const {deckID} = useParams();
-    console.log(deckID);
+    useEffect(()=>{
+        
+        async function getDeck(){
+           
+            try{
+            const response = await readDeck(deckID);
+            setDeck(response);
+                // console.log('EditDeck deck::::', deck);
+            }
+            catch(e){
+                console.log('EditDeck getDeck error:: ', e);
+            }
+        }
+        getDeck();
+    },[deckID]);
+
+    console.log('EditDeck deck_outside::::', deck);
 
     const EditDeckNaviLink =()=>{
         return(
             <nav aria-label="breadcurmb">
             <ol className="breadcrumb">
                 <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                <li className="breadcrumb-item">Deckname</li>
+                <li className="breadcrumb-item">{deck.name}</li>
             </ol>
         </nav>
         )
     }
+    if(deck){
     return(
         <>
             <EditDeckNaviLink />
             <h1>Edit Deck</h1>
+            <CreateDeckForm deck={deck}/>
          </>
     )
+}
+return null;
 }
 
 export default EditDeck;
