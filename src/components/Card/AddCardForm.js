f7edef94bb38d33c5f0082857c55d82dbacb7999
kneerace <kneerace@gmail.com>
2022-06-27 06:40:33 -0400

    AddCardForm>> updated to include Edit to it.

--- src/components/Card/AddCardForm.js
+++ src/components/Card/AddCardForm.js
@@ -1,14 +1,37 @@
-import React, { useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import { createCard } from "../../utils/api";
+import React, { useState,useEffect } from "react";
+import { useHistory, useLocation, useParams } from "react-router-dom";
+import { createCard, readCard, updateCard } from "../../utils/api";
 
 function AddCardForm({deck}){
 
         // console.log("AddCardForm deck::prop:: ",deck);
     const history = useHistory();
-    const {deckID} = useParams();
+    const {deckID, cardId} = useParams();
+    const {pathname} = useLocation();
+
     const [front, setFront] = useState({"front":""})
     const [back, setBack] = useState({"back":""});
+    const [isEdit, setIsEdit] = useState(false);
+
+
+    const abort = new AbortController()
+    useEffect(()=> {
+        async function fetchCard(){
+            const response = await readCard(cardId,abort.signal);
+            console.log('EditCardForm readCard response:::', response)
+            setFront({"front":response.front});
+            setBack({"back":response.back});
+        }
+        function editAdd(){
+            if(pathname.includes("edit")){
+                setIsEdit(true);
+                fetchCard();
+            }
+        }
+        editAdd();
+    },[cardId,pathname]);
+
+        console.log("AddCardform  isEdit::::", isEdit);
 
     const handleFront = (event) =>{
         event.preventDefault();
@@ -26,10 +49,15 @@ function AddCardForm({deck}){
         setBack({"back":""})
     }
 
-    const handleDone = () =>{
+    const handleCancelDone = () =>{
         history.push(`/decks/${deckID}`);
     }
 
+    function handleUpdate(){
+        updateCard({"id":cardId, "deckId":parseInt(deckID), ...front, ...back})
+        .then(history.push(`/decks/${deckID}`))
+        .catch((e)=> console.log("EditCardForm UpdateCard Error: ", e));
+     }
 
     return(
         <>
@@ -44,8 +72,8 @@ function AddCardForm({deck}){
                     <textarea className="form-control" id='back' rows="3" placeholder="Back side of card"
                         value={back.back} onChange={handleBack}></textarea>
                 </div>
-                <button className="btn btn-secondary mr-1" onClick={handleDone}>Done</button>
-                <button className="btn btn-primary" onClick={handleSave}>Save</button>
+                <button className="btn btn-secondary mr-1" onClick={handleCancelDone}>{isEdit ? "Cancel" : "Done"}</button>
+                <button className="btn btn-primary" onClick={isEdit ? handleUpdate : handleSave}>{isEdit ? "Submit" : "Save"}</button>
             </form>
         </>
     )
